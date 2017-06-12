60baaebfbbcc27b7f2e1a85f01453676d094dc55
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-12 11:43:06 -0500

    feat: add App component for landing page

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MyTitle = function(props) {
-  return React.createElement('div', null, React.createElement('h1', { style: { color: props.color } }, props.title));
-};
+const App = () => (
+  <div className='app'>
+    <div className='landing'>
+      <h1>react app v3</h1>
+      <input type="text" placeholder="Search"/>
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyFirstComponent = function() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(MyTitle, {
-      title: 'Game of Thrones',
-      color: 'yellowgreen'
-    }),
-    React.createElement(MyTitle, {
-      title: 'Stranger Things',
-      color: 'greenyellow'
-    }),
-    React.createElement(MyTitle, {
-      title: 'Rick and Morty',
-      color: 'limegreen'
-    })
-  );
-};
-
-render(React.createElement(MyFirstComponent), document.getElementById('app'));
+render(<App/>, document.getElementById('app'));
