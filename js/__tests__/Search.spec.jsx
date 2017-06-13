22a0202c4156000b6309fd87e81d6d9fd1a48f92
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-13 10:53:21 -0500

    feat: add testing with jest and enzyme

---
+++ js/__tests__/Search.spec.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import preload from '../../data.json';
+import Search from '../Search';
+import ShowCard from '../ShowCard';
+
+describe('Search', () => {
+
+  it('renders correctly', () => {
+    const component = shallow(<Search/>);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correct amount of shows', () => {
+    const component = shallow(<Search/>);
+
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+  });
+
+  it('should render correct amount of shows based on search term', () => {
+    const searchTerm = 'black';
+    const component = shallow(<Search/>);
+
+    component.find('input').simulate('change',{target: {value: searchTerm}});
+    const showCount = preload.shows.filter(show => `${show.title.toLowerCase()} ${show.description.toLowerCase()}`.indexOf(searchTerm.toLowerCase()) > -1).length;
+
+    expect(component.find(ShowCard).length).toEqual(showCount);
+  });
+});
