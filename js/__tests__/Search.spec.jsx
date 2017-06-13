6d7218c3b20f98d66f4a79b76d7f8e7d513ac88a
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-13 18:12:02 -0500

    feat: create Details component

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -9,20 +9,20 @@ import ShowCard from '../ShowCard';
 describe('Search', () => {
 
   it('renders correctly', () => {
-    const component = shallow(<Search />);
+    const component = shallow(<Search shows={preload.shows}/>);
 
     expect(component).toMatchSnapshot();
   });
 
   it('should render correct amount of shows', () => {
-    const component = shallow(<Search />);
+    const component = shallow(<Search shows={preload.shows}/>);
 
     expect(component.find(ShowCard).length).toEqual(preload.shows.length);
   });
 
   it('should render correct amount of shows based on search term', () => {
     const searchTerm = 'black';
-    const component = shallow(<Search />);
+    const component = shallow(<Search shows={preload.shows}/>);
     component.find('input').simulate('change', { target: { value: searchTerm } });
     const showCount = preload.shows.filter(
       show => `${show.title} ${show.description}`.toLowerCase().indexOf(searchTerm.toLowerCase()) >-1
