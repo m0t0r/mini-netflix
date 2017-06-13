8c497bb5b57938b87d31f5ef181dc770f3223ed1
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-12 23:11:48 -0500

    feat: make the search actually work

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className='search'>
-    {preload.shows.map(show => <ShowCard show={show}/>)}
-  </div>
-);
+class Search extends Component {
+
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTerm = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className='search'>
+        <header>
+          <h1>react app v3</h1>
+          <input onChange={this.handleSearchTerm} value={this.state.searchTerm} type="text" placeholder="Search"/>
+        </header>
+        <div>
+          {
+            preload.shows
+              .filter(show => `${show.title} ${show.description}`.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) > -1)
+              .map(show => <ShowCard key={show.imdbID} show={show}/>)
+          }
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default Search;
