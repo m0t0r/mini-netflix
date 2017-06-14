5e0b3e057eee3d9066552f511bb0c12db3f1f3e1
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-14 12:01:46 -0500

    refactor: migrate Search and Landing into redux

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,12 +1,14 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setSearchTermAction } from './redux/action-creators';
 
 const Header = (props: {
   showSearch?: boolean,
-  handleSearchTermChange?: Function,
-  searchTerm?: string
+  handleSearchTermChange: Function,
+  searchTerm: string
 }) => {
 
   let utilSpace;
@@ -38,9 +40,17 @@ const Header = (props: {
 };
 
 Header.defaultProps = {
-  showSearch: false,
-  searchTerm: '',
-  handleSearchTermChange: () => {}
+  showSearch: false
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange (event) {
+    dispatch(setSearchTermAction(event.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,41 +1,30 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import ShowCard from './ShowCard';
 import Header from './Header';
+import { setSearchTermAction } from './redux/action-creators';
 
-class Search extends Component {
-  state = {
-    searchTerm: ''
-  };
+const Search = (props: {
+  searchTerm: string, // eslint-disable-line react/no-unused-prop-types
+  shows: Array<Show>
+}) => (
+  <div className="search">
+    <Header showSearch />
+    <div>
+      {props.shows
+        .filter(
+          show =>
+          `${show.title} ${show.description}`.toLowerCase().indexOf(props.searchTerm.toLowerCase()) > -1
+        )
+        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
+  </div>
+);
 
-  props: {
-    shows: Array<Show>
-  };
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
 
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & {target: HTMLInputElement}) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-
-  render() {
-    return (
-      <div className="search">
-        <Header
-          showSearch
-          searchTerm={this.state.searchTerm}
-          handleSearchTermChange={this.handleSearchTermChange}
-        />
-        <div>
-          {this.props.shows
-            .filter(
-              show =>
-                `${show.title} ${show.description}`.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) > -1
-            )
-            .map(show => <ShowCard key={show.imdbID} {...show} />)}
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Search;
+export default connect(mapStateToProps)(Search);
