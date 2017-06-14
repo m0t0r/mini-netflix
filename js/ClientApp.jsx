e25cb87995332a290fad98c2c59179a90889097d
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-14 09:28:27 -0500

    chore: add perf profiling tool

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,6 +3,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+// import Perf from 'react-addons-perf';
+
+// Performance profiling
+//
+// window.Perf = Perf;
+// Perf.start();
 
 const renderApp = () => {
   render(<App />, document.getElementById('app'));
