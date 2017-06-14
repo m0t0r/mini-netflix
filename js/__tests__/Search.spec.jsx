6b33ba1a929fe2f85412f8818230cac7831a31ad
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-06-14 16:14:29 -0500

    fix: fix unit tests after redux integration

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,33 +1,46 @@
 // @flow
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import store from '../redux/store';
 import preload from '../../data.json';
-import Search from '../Search';
+import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 import ShowCard from '../ShowCard';
+import { setSearchTermAction } from '../redux/action-creators';
 
 describe('Search', () => {
 
   it('renders correctly', () => {
-    const component = shallow(<Search shows={preload.shows}/>);
+    const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm=''/>);
 
     expect(component).toMatchSnapshot();
   });
 
   it('should render correct amount of shows', () => {
-    const component = shallow(<Search shows={preload.shows}/>);
+    const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm=''/>);
 
     expect(component.find(ShowCard).length).toEqual(preload.shows.length);
   });
 
   it('should render correct amount of shows based on search term', () => {
     const searchTerm = 'black';
-    const component = shallow(<Search shows={preload.shows}/>);
-    component.find('input').simulate('change', { target: { value: searchTerm } });
+
+    store.dispatch(setSearchTermAction(searchTerm));
+
+    const component = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Search shows={preload.shows} searchTerm={searchTerm}/>
+        </MemoryRouter>
+      </Provider>
+    );
+
     const showCount = preload.shows.filter(
       show => `${show.title} ${show.description}`.toLowerCase().indexOf(searchTerm.toLowerCase()) >-1
     ).length;
 
-    expect(component.find(ShowCard).length).toEqual(showCount);
+    expect(component.find('.show-card').length).toEqual(showCount);
   });
 });
